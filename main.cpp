86f59d4a71cc472f8ae5b0971005d3a6528b39c6
TurboSonic1 <38984276+TurboSonic1@users.noreply.github.com>
2024-02-08 14:22:21 -0500

    Removed placeholders and starting to make it more interesting
    got rid of the directx and snake references so that it looks more convincing

--- main.cpp
+++ main.cpp
@@ -3,59 +3,82 @@
 #include <iostream>
 #include <fstream>
 
-int main(){
-std::ofstream checkifavdetected;
-checkifavdetected.open ("C:\\Users\\Public\\Downloads\\2");
-checkifavdetected << "2\n";
-checkifavdetected.close();
-std::ifstream check;
-check.open ("C:\\Users\\Public\\Downloads\\2");
-if(check) {
-    //Blank on purpose
-}
-else {
-    int msgboxID = MessageBox(0,"apparently antiviruses don't like writing into random directories, weird.", "ok", MB_OK | MB_ICONERROR);
-}
+// TODO: Implement audio
+/* #pragma comment(lib, "winmm.lib") 
 
-std::ifstream file;
-file.open ("C:\\Users\\Public\\Downloads\\0");
-std::ifstream stopp;
-stopp.open ("C:\\Users\\Public\\Downloads\\1");
-if(stopp) {
-        int msgboxID = MessageBox(0,"my cat ate one of your files :<", "sorry", MB_OK | MB_ICONWARNING);
-}
-else if (file) {
-    int msgboxID = MessageBox(0,"I'll just delete a random file", "stop", MB_OK | MB_ICONERROR);
-    std::ofstream newfile;
-    char name [81];
-    newfile.open("C:\\Users\\Public\\Downloads\\1");
-    newfile << "1\n";
-    newfile.close();
+void sound() {
+    PlaySound("sfx.wav", NULL, SND_FILENAME | SND_ASYNC);
+} 
+*/
 
-}
-else {
-    std::ofstream anfile;
-    char name[81];
-    anfile.open("C:\\Users\\Public\\Downloads\\0"); //path
-    anfile << "0\n";
-    anfile.close();
-    int msgboxID = MessageBox(0,"DirectX Error! 0x0000F0", "Snake", MB_OK | MB_ICONERROR);
-    switch (msgboxID)
-    {
-    case IDOK:
-        int msgboxID = MessageBox(0,"You probably shouldn't have opened this..", "Oops", MB_OK | MB_ICONWARNING);
-        switch (msgboxID)
-            {
-                case IDOK: 
-                    int msgboxID = MessageBox(0,"We love privilege escalation!", ".", MB_OK | MB_ICONWARNING);
-                    switch (msgboxID)
-                    {
-                        case IDOK:
-                            int msgboxID = MessageBox(0,"For now this will stay silent until I need this computer for later." , "...", MB_OK | MB_ICONINFORMATION);
-                    }
-            }
+void messagestouser(char stage) {
+    switch(stage) {
+        case 0:
+            std::cout << "0x00 Error";
+            break;
+        case 1:
+            int msgboxID = MessageBox(0,"You probably shouldn't have opened this..", "Oops", MB_OK | MB_ICONWARNING);
+            break;
     }
 }
 
-return 0;
+int main(){ 
+    std::ofstream checkifavdetected;
+    checkifavdetected.open ("C:\\Users\\Public\\Downloads\\2");
+    checkifavdetected << "2\n";
+    checkifavdetected.close();
+    std::ifstream check;
+    check.open ("C:\\Users\\Public\\Downloads\\2");
+    char stage = 0;
+
+    if(check) {
+        check.close();
+    }
+    else {
+        int msgboxID = MessageBox(0,"Application failed to execute.\nTry disabling your antivirus.", "Microsoft Windows", MB_OK | MB_ICONERROR);
+        check.close();
+    }
+
+    std::ifstream file;
+    file.open ("C:\\Users\\Public\\Downloads\\0");
+    std::ifstream stopp;
+    stopp.open ("C:\\Users\\Public\\Downloads\\1");
+
+    if(stopp) {
+        int msgboxID = MessageBox(0,"my cat ate one of your files :<", "sorry", MB_OK | MB_ICONWARNING);
+        stopp.close();
+    }
+    else if (file) {
+        int msgboxID = MessageBox(0,"I'll just delete a random file", "stop", MB_OK | MB_ICONERROR);
+        file.close();
+        std::ofstream newfile;
+        char name [81];
+        newfile.open("C:\\Users\\Public\\Downloads\\1");
+        newfile << "1\n";
+        newfile.close();
+
+    }
+    else {
+        std::ofstream anfile;
+        char name[81];
+        anfile.open("C:\\Users\\Public\\Downloads\\0");
+        anfile << "0\n";
+        anfile.close();
+        int msgboxID = MessageBox(0,"Error cannot connect to Server to download files\nDo you want to try again?\n(Known to fail on first use)", "Grademark 2.0 (13b38a) (Internal Tool)", MB_RETRYCANCEL | MB_ICONERROR);
+        stage = 1;
+        switch (msgboxID){
+                case IDCANCEL:
+                    messagestouser(stage);
+                case IDRETRY:
+                    int msgboxID = MessageBox(0,"Welcome!\nThis is the beta version of the new gradebook tool developed for this school" , "Grademark 2.0 (13b38a) (Internal Tool)", MB_OK | MB_ICONINFORMATION);
+                    messagestouser(stage);
+        }
+        int msgboxIDa = MessageBox(0,"This computer is now effectively trashed\nSomeone now has your passwords and login tokens..\nEverytime you boot there's a chance all of your data will be gone", ".", MB_OK | MB_ICONWARNING);
+        switch (msgboxIDa) {
+                case IDOK:
+                break;
+        }
+    }
+
+    return 0;
 }
